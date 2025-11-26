d1234571cbbbc6ec4488d8c5bbad98b9466496af
parkjunesong <parkjunesong@gmail.com>
2025-11-26 16:40:20 +0900

    dd

--- test.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.listen(8080, function(){console.log("8080 waiting")})
-app.get('/hello', function(req, res){res.send('hello world!');})
